19e341a764ae1340eb1087ff8d8959ad8939acf5
Santiago Pereira <97372983+SRV-JSPA@users.noreply.github.com>
2023-05-22 17:03:50 -0600

    actualizaciones

--- LAB9/LAB9/LAB9.asm
+++ LAB9/LAB9/LAB9.asm
@@ -13,5 +13,14 @@ includelib legacy_stdio_definitions.lib; importancion de librerias
 .model flat, stdcall, C ; direccion de memoria y llamado de funciones 
 
 printf proto c: vararg; definicion de printf
+ExitProcess proto,dwExitCode:dword
 
 .data
+
+  msg1 BYTE 'Ingrese el mes de facturación: ',0
+  msg2 BYTE 'Ingrese el nit para la facturación: ',0
+  msg3 BYTE 'Ingrese el monto facturado: ',0
+  strBuff BYTE 255 DUP(?)
+  fmt db "%s",0
+  
+  

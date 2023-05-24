0b705d898629aefc06f490226294c4a3fca0e52b
Santiago Pereira <97372983+SRV-JSPA@users.noreply.github.com>
2023-05-23 20:52:37 -0600

    :)

--- LAB9/LAB9/LAB9.asm
+++ LAB9/LAB9/LAB9.asm
@@ -5,56 +5,7 @@
 ;Descripcion: Laboratorio 9
 ;24/05/2023
 
-.386
-.model flat, stdcall, c
-.stack 4096
-ExitProcess proto,dwExitCode:dword
 
-.data
-
-  msg1 BYTE 'Ingrese el mes de facturación: ',0
-  msg2 BYTE 'Ingrese el nit para la facturación: ',0
-  msg3 BYTE 'Ingrese el monto facturado: ',0
-  buffer BYTE 255 DUP(?)
-  fmt db "%s",0
- 
- 
-.code
-
- includelib libucrt.lib
- includelib legacy_stdio_definitions.lib
- includelib libcmt.lib
- includelib libvcruntime.lib
-
- extrn printf:near
- extrn scanf:near
- extrn exit:near
-
- public main
- main proc
-
- 	push ebp 
-	mov ebp, esp
-
-	push offset msg1
-	call printf
-
-	lea eax, buffer
-	push eax
-	push offset fmt 
-	call scanf
-
-	push offset buffer
-	call printf
-
-	add esp, 8
-	pop ebp
-
-	push 0
-    call exit ;
-main endp
-
-end
 
 
 
